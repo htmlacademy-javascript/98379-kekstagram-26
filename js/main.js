81f16950884343bf42c8de72abb8f2598d733cef
Bizikin <robi90@mail.ru>
2022-07-11 03:39:56 +0300

    Task 7.2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 import './data.js';
 import './midget.js';
-// import './fullscreen.js';
 
 
--- js/midget.js
+++ js/midget.js
@@ -1,5 +1,7 @@
 import { createPublication } from './data.js';
 
+// ПЕРВЫЙ БЛОК 7 ЗАДАНИЯ
+
 const pictures = document.querySelector('.pictures');
 const templatePicture = document.querySelector('#picture').content;
 const template = templatePicture.querySelector('a');
@@ -22,9 +24,10 @@ pictures.appendChild(fragment);
 
 const smallPicture = document.querySelectorAll('.picture');
 const bigPicture = document.querySelector('.big-picture');
+const bigPictureCancelBtn = document.querySelector('.big-picture__cancel');
 
-// const bigPictureCancelBtn = document.querySelector('.big-picture__cancel');
-
+// 2.Для отображения окна нужно удалять класс hidden у элемента .big-picture и каждый раз заполнять его данными о конкретной фотографии
+// Для решения данной задачи и последней(связки миниатюры и полноразмерного формата) создаётся цикл, где "в лоб" массивы обмениваются данными
 
 for (let i = 0; i < smallPicture.length; i++) {
   smallPicture[i].addEventListener('click', function() {
@@ -32,11 +35,44 @@ for (let i = 0; i < smallPicture.length; i++) {
     bigPicture.querySelector('.big-picture__img img').src = this.querySelector('.picture__img').src;
     bigPicture.querySelector('.likes-count').textContent = this.querySelector('.picture__likes').textContent;
     bigPicture.querySelector('.comments-count').textContent = this.querySelector('.picture__comments').textContent;
+    const commentsObj = arrayPicture[i].comments;
+    const descObj = arrayPicture[i].description;
+    const comment1 = commentsObj[0];
+    const comment2 = commentsObj[1];
 
-    bigPicture.querySelector('.social__text').textContent = arrayPicture[i].message;
+    const bigPictureComments = bigPicture.querySelectorAll('.social__comment');
+    bigPictureComments[0].getElementsByTagName('img')[0].src = comment1[1].avatar;
+    bigPictureComments[0].getElementsByTagName('p')[0].textContent = comment1[2].message;
+    bigPictureComments[0].getElementsByTagName('img')[0].alt = comment1[3].name;
+
+    bigPictureComments[1].getElementsByTagName('img')[0].src = comment2[1].avatar;
+    bigPictureComments[1].getElementsByTagName('p')[0].textContent = comment2[2].message;
+    bigPictureComments[1].getElementsByTagName('img')[0].alt = comment2[3].name;
+
+    bigPicture.querySelector('.social__caption').textContent = descObj;
+
+    // 3. После открытия окна спрячьте блоки счётчика комментариев .social__comment-count
+    // и загрузки новых комментариев .comments-loader, добавив им класс hidden
+    bigPicture.querySelector('.social__comment-count').classList.add('hidden');
+    bigPicture.querySelector('.comments-loader').classList.add('hidden');
+
+    // 4. После открытия окна добавьте тегу <body> класс modal-open, чтобы контейнер с фотографиями
+    // позади не прокручивался при скролле. При закрытии окна не забудьте удалить этот класс.
+    document.querySelector('body').classList.add('modal-open');
+
   });
 }
 
-// bigPictureCancelBtn.addEventListener('click', function() {
-//   bigPicture.classList.add('hidden');
-// });
+// 5. Напишите код для закрытия окна по нажатию клавиши Esc и клике по иконке закрытия.
+
+bigPictureCancelBtn.addEventListener('click', () => {
+  bigPicture.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+});
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.keyCode === 27) {
+    bigPicture.classList.add('hidden');
+    document.querySelector('body').classList.remove('modal-open');
+  }
+});
