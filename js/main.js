6f78db8d4558ec6ce8ea7fedc3673554eab36ab6
Bizikin <robi90@mail.ru>
2022-07-15 16:42:02 +0300

    Task 8.1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { createPublication } from './data.js';
 import { renderMiniatures } from './miniatures.js';
+import './form.js';
 
 const publicationData = Array.from({ length: 6 }, createPublication);
 renderMiniatures();
---
+++ js/util.js
@@ -0,0 +1,15 @@
+const isEscPressed = (evt) => evt.key === 'Escape';
+
+const checkStringLength = function(string, maxLength) {
+  return string.length <= maxLength;
+};
+
+function getRandomPositiveInteger(a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+
+export { isEscPressed, getRandomPositiveInteger, checkStringLength };
