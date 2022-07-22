9ee854b84bdebb0e4b8aad1ce8c54827cdaea1ee
Bizikin <robi90@mail.ru>
2022-07-22 03:37:56 +0300

    Task 11.1

--- js/util.js
+++ js/util.js
@@ -1,3 +1,5 @@
+const ALERT_SHOW_TIME = 4000;
+
 const isEscPressed = (evt) => evt.key === 'Escape';
 
 const checkStringLength = function(string, maxLength) {
@@ -11,5 +13,26 @@ function getRandomPositiveInteger(a, b) {
   return Math.floor(result);
 }
 
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = 0;
+  alertContainer.style.top = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '15px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
 
-export {isEscPressed, getRandomPositiveInteger, checkStringLength};
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+
+export {isEscPressed, getRandomPositiveInteger, checkStringLength, showAlert};
